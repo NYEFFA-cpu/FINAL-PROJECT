27716671dfe0f30f0e0f031e569dc0487731fabd
martin <martinmmuriithi28@gmail.com>
2026-08-07 02:57:48 +0300

    Fix null addEventListener error in shared JavaScript

--- script.js
+++ script.js
@@ -96,16 +96,21 @@ viewButtons.forEach(button => {
 });
 
 // Close Modal
-closeModal.addEventListener("click", () => {
-    modal.style.display = "none";
-});
+
+if (closeModal && modal) {
+    closeModal.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+}
 
 // Click Outside Modal
-window.addEventListener("click", e => {
-    if(e.target === modal){
-        modal.style.display = "none";
-    }
-});
+if (modal) {
+    window.addEventListener("click", e => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+}
 
 // Scroll To Top Button
 window.addEventListener("scroll", () => {
