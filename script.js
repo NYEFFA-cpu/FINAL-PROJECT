79265e9de63c1eea01053ed673bfb8fdf8f37c85
ezra5377 <ezra.kunyiha@gmail.com>
2026-08-06 13:27:34 +0300

    click animation

--- script.js
+++ script.js
@@ -40,3 +40,13 @@
     }
     nameinput.addEventListener("input", validateform)
     emailinput.addEventListener("input", validateform)
+
+    // click animation 
+     button.addEventListener("click",()=>{
+
+        button.style.transform="scale(0.9)";
+
+        setTimeout(() => {
+            button.style.transform = "scale(1)";
+        },100);
+     });
